eb497d4cb9292e08cf3976ff2dc8f607396e6a7f
Redwi-v <forest45456@gmail.com>
2024-04-20 10:25:01 +0300

    edits

--- src/app/(pages)/delivery.and.pay/page.tsx
+++ src/app/(pages)/delivery.and.pay/page.tsx
@@ -1,8 +1,12 @@
 'use client'
 import { H1, H2, P } from "@/shared/ui.kit"
 import { NextPage } from "next"
-import s from './contacts.module.scss'
+import s from './delivery.and.pay.module.scss'
 import { Map, YMaps } from '@pbe/react-yandex-maps';
+import { useState } from "react";
+import { cssIf } from "@/shared/scripts";
+import { MapPointImage } from "@/app/_images/map.point";
+import { ClockImage } from "@/app/_images/clock";
 
 interface ContactsPageProps {
 
@@ -10,10 +14,106 @@ interface ContactsPageProps {
 
 const ContactsPage: NextPage<ContactsPageProps> = props => {
 
+  const [ pointsActive, setPointsActive ] = useState( false )
+
   return (
 
     <section className={ `${ s.delivery_and_pay } container` }>
 
+      <H1 className={ `${ s.title } text-left` }>Доставка и оплата</H1>
+
+      <div className={ `${ s.zone } mt-24 flex items-center` }>
+
+        <H2 className="">Зоны покрытия и кофейни</H2>
+
+        <div onClick={ () => setPointsActive( prev => !prev ) } className={ s.toggle }>
+
+          <span className={ `${ cssIf( !pointsActive, s.active ) }` } >Зона доставки</span>
+          <span className={ `${ cssIf( pointsActive, s.active ) }` } >Точки самовывоза</span>
+
+        </div>
+
+      </div>
+
+      <P className={ `${ s.address } flex gap-8 mt-16 items-center` }>
+        <MapPointImage />
+        Москва, 3-я улица Строителей, 25
+      </P>
+
+      <P className={ `${ s.time } flex gap-8 mt-16 items-center` }>
+        <ClockImage />
+        Время работы заведения: с 10:00 до 21:00
+      </P>
+
+
+      <YMaps >
+        <div className={ `${ s.map } mt-24` }>
+          <Map width="100%" height="100%" defaultState={ { center: [ 55.685031, 37.537362 ], zoom: 15 } } >
+          </Map>
+        </div>
+      </YMaps>
+
+      <H2 className="mt-60">Доставка</H2>
+
+      <P className={ `${ s.text } mt-24` }>
+
+        Доставка заказов производится с 9:00 до 22:00 по Санкт-Петербургу в рамках «серой зоны». Способ получения заказа и временной интервал вы выбираете при оформлении заказа.
+        
+      </P>
+
+      <P className={ s.text }>
+
+        Сумма заказа до 499 руб. – доставка 299 рублей;<br />
+        Сумма заказа 500-999 руб. – доставка 229 рублей;<br />
+        Сумма заказа 1.000-1.999 руб. – доставка 149 рублей;<br />
+        Сумма заказа от 2.000 руб. – бесплатная доставка.
+
+      </P>
+
+      <P className={ s.text }>
+        Курьерская доставка в близлежащие районы города за пределами «серой зоны»
+        по согласованию с оператором и оплачивается дополнительно.
+        Заказы, оформленные после 17:00, доставляются на следующий день.
+        В день доставки просим вас оставаться на связи по указанному в заказе номеру телефона, чтоб оператор интернет-магазина мог связаться с вами. В комментариях
+        к заказу можно указать удобное время для звонка.
+        Оформленный заказ также можно забрать в наших кафе. Самовывоз – бесплатно.
+      </P>
+
+      <H2 className={ `mt-60` }>оплата</H2>
+
+      <P className={ `${ s.text } mt-24` }>
+        Возможные способы оплаты меняются в зависимости от вашего адреса, а также способа и  времени доставки. Увидеть все доступные способы оплаты и выбрать самый удобный вы можете в  корзине.
+      </P>
+
+
+      <ul className={ s.pay_methods }>
+
+        <li className={ s.pay_method }>
+          <H2>Картой на сайте</H2>
+
+          <P className={ `${ s.text }` }>
+            Привяжите банковскую карту, чтобы оплачивать покупки онлайн.
+          </P>
+        </li>
+
+        <li className={ s.pay_method }>
+          <H2>Картой курьеру</H2>
+
+          <P className={ `${ s.text }` }>
+            Выберите эту опцию, если хотите оплатить покупку картой при  получении. Курьер возьмеёт терминал.
+          </P>
+        </li>
+
+
+        <li className={ s.pay_method }>
+          <H2>Наличными</H2>
+
+          <P className={ `${ s.text }` }>
+            Вы можете оплатить заказ наличными при получении, если такой способ оплаты доступен по вашему адресу.
+          </P>
+        </li>
+
+      </ul>
 
     </section>
 
---
+++ src/app/_images/map.point.tsx
@@ -0,0 +1,7 @@
+export const MapPointImage = ( { className }: { className?: string } ) => (
+  <svg className={ className } width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M13.9816 11.9842C15.0411 10.804 15.0216 9.2785 13.9816 8.1945C12.2034 6.34108 8.6228 7.90607 9.31909 10.4901C9.98471 12.9603 12.429 13.7136 13.9816 11.9842Z" stroke="#37363F" stroke-width="1.8" stroke-linecap="round" />
+    <path d="M3.13253 9.52738C2.91655 13.9683 9.19339 20.0936 12.3588 22.6011C13.8539 21.318 20.26 14.7632 20.2312 9.86014C20.2022 4.91093 16.2701 2.14615 12.1028 1.82835C7.93553 1.51056 3.4025 3.97628 3.13253 9.52738Z" stroke="#37363F" stroke-width="1.8" stroke-linecap="round" />
+  </svg>
+
+)
